09e4d7785ef59a45f9ee5c96bae10daad5b11b02
zainglenn <zainglenndvt@gmail.com>
2018-04-12 14:42:43 +0200

    Signed-off-by: zainglenn <zainglenndvt@gmail.com> Added to gitignore and pushing the spring boot project

---
+++ springFSSkeletonProject/src/main/java/com/springFSSkeletonProject/SpringFsSkeletonProjectApplication.java
@@ -0,0 +1,12 @@
+package com.springFSSkeletonProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFsSkeletonProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFsSkeletonProjectApplication.class, args);
+	}
+}
