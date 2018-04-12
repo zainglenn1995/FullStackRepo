9cf21deca5cd04dadfd9c99a997de679ecf68e28
zainglenn <zainglenndvt@gmail.com>
2018-04-12 14:54:15 +0200

    Signed-off-by: zainglenn <zainglenndvt@gmail.com> HEllo world template and java controller

---
+++ springFSSkeletonProject/src/main/java/com/springFSSkeletonProject/controllers/HelloWorlController.java
@@ -0,0 +1,13 @@
+package com.springFSSkeletonProject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorlController {
+	
+	@RequestMapping("/")
+	public String sayHello() {
+		return "index";
+	}
+}
